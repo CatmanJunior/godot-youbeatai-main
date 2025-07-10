d5fe58811fe59edb104b10865229a2470ec57382
Jesse van Leeuwen <jesse@emberglitch.com>
2025-07-10 13:30:19 +0200

    code: animation timings changed delay timer no longer needed

--- Experimental/Klappy/klappy_animations.gd
+++ Experimental/Klappy/klappy_animations.gd
@@ -1,8 +1,6 @@
 extends Node3D
 
 @onready var animation_tree = $model/AnimationTree
-@onready var stamp_timer = $Stamp
-@onready var clap_timer = $Clap
 
 var beat_time = 0.0
 
@@ -14,19 +12,9 @@ func _ready():
 	# default speed for 120 bpm
 	if beat_time == 0:
 		on_bpm_changed(120.0)
-	
-	var on_clap_timer := func(): 
-		animation_tree.set("parameters/ClapTrigger/seek_request", 0)
-	clap_timer.timeout.connect( on_clap_timer )
-	
-	var on_stamp_timer := func():
-		animation_tree.set("parameters/StampTrigger/seek_request", 0)
-	stamp_timer.timeout.connect(on_stamp_timer)
 
 func init():
 	animation_tree = $model/AnimationTree
-	stamp_timer = $Stamp
-	clap_timer = $Clap
 
 func _input(event):
 	if event is InputEventKey and event.pressed:
@@ -36,20 +24,18 @@ func _input(event):
 
 # trigger clap animation by setting time to 0.0
 func on_clap():
-	clap_timer.start()
+	animation_tree.set("parameters/ClapTrigger/seek_request", 0)
+	#clap_timer.start()
 
 # trigger stamp animation by setting time to 0.0
 func on_stamp():
-	stamp_timer.start()
+	animation_tree.set("parameters/StampTrigger/seek_request", 0)
 
 # adjust animation speed to match bpm
 func on_bpm_changed(bpm:float):
-	# catch for when bpm change is called before onready
-	if not clap_timer:
+	if not animation_tree:
 		init()
-	
+	# catch for when bpm change is called before onready
 	beat_time = bpm / 60.0
 	var beat_time_half = (1.0 / beat_time) * 0.25	
-	clap_timer.wait_time = beat_time_half
-	stamp_timer.wait_time = beat_time_half
 	animation_tree.set("parameters/TimeScale/scale", beat_time )

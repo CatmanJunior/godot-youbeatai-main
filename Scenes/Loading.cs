26e254218bd117a994abf76931a720758abc9303
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2025-07-15 12:46:19 +0200

    async scene loading in loading screen

--- Scenes/Loading.cs
+++ Scenes/Loading.cs
@@ -5,9 +5,31 @@ public partial class Loading : Panel
 {
     bool firstframedone = false;
 
-    public override void _Process(double delta)
+    public override async void _Process(double delta)
     {
-        if (firstframedone) GetTree().ChangeSceneToFile("res://Scenes/main.tscn");
-        else firstframedone = true;
+        UpdateLoadingAnimation();
+
+        if (!firstframedone)
+        {
+            firstframedone = true;
+            return;
+        }
+
+        // cache tree
+        var tree = GetTree();
+
+        // load main scene async
+        ResourceLoader.LoadThreadedRequest("res://Scenes/main.tscn");
+        bool isloaded = ResourceLoader.LoadThreadedGetStatus("res://Scenes/main.tscn") == ResourceLoader.ThreadLoadStatus.Loaded;
+        while (!isloaded) await ToSignal(tree, SceneTree.SignalName.ProcessFrame);
+
+        // switch scene
+        var packedScene = (PackedScene)ResourceLoader.LoadThreadedGet("res://Scenes/main.tscn");
+        tree.ChangeSceneToPacked(packedScene);
+    }
+
+    private void UpdateLoadingAnimation()
+    {
+        // updates animation each frame
     }
 }

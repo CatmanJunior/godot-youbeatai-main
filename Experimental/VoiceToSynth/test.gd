fb334f2940e2f4aa097e6be768f3d89662f34997
Jesse van Leeuwen <jesse@emberglitch.com>
2025-07-15 18:00:02 +0200

    fix: test scene script to work outside of main scene

---
+++ Experimental/VoiceToSynth/test.gd
@@ -0,0 +1,16 @@
+extends Control
+
+var recorder: AudioEffectRecord
+
+func _ready():
+	# We get the index of the "Record" bus.
+	var idx = AudioServer.get_bus_index("Microphone")
+	# And use it to retrieve its first effect, which has been defined
+	# as an "AudioEffectRecord" resource.
+	recorder = AudioServer.get_bus_effect(idx, 1) 
+
+func start():
+	recorder.set_recording_active(true)
+	
+func stop():
+	recorder.set_recording_active(false)

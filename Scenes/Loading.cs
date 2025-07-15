be70c5f61a16f8b71a488b3e06d9c24a4dab5936
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2025-07-15 13:11:02 +0200

    better loading

--- Scenes/Loading.cs
+++ Scenes/Loading.cs
@@ -4,26 +4,42 @@ using System;
 public partial class Loading : Panel
 {
     bool firstframedone = false;
+    bool startedloading = false;
 
-    public override async void _Process(double delta)
+    public override void _Process(double delta)
     {
         UpdateLoadingAnimation();
 
-        if (!firstframedone)
+        if (firstframedone)
+        {
+            if (!startedloading)
+            {
+                StartAsyncLoad();
+                startedloading = true;
+            }
+        }
+        else
         {
             firstframedone = true;
-            return;
         }
+    }
 
-        // cache tree
+    async void StartAsyncLoad()
+    {
         var tree = GetTree();
 
-        // load main scene async
+        // start the async loading
         ResourceLoader.LoadThreadedRequest("res://Scenes/main.tscn");
-        bool isloaded = ResourceLoader.LoadThreadedGetStatus("res://Scenes/main.tscn") == ResourceLoader.ThreadLoadStatus.Loaded;
-        while (!isloaded) await ToSignal(tree, SceneTree.SignalName.ProcessFrame);
 
-        // switch scene
+        // loop to check if loading async is done
+        bool isloaded = false;
+        while (!isloaded)
+        {
+            isloaded = ResourceLoader.LoadThreadedGetStatus("res://Scenes/main.tscn") == ResourceLoader.ThreadLoadStatus.Loaded;
+            await ToSignal(tree, SceneTree.SignalName.ProcessFrame);
+        }
+
+        // if async loading is done, switch the scene
         var packedScene = (PackedScene)ResourceLoader.LoadThreadedGet("res://Scenes/main.tscn");
         tree.ChangeSceneToPacked(packedScene);
     }

3bd1900818ab9c79135ff9aa5d5781b866de526f
MelodyTheMuse <keanodussel@gmail.com>
2025-09-30 12:59:52 +0200

    Added todo comments for what needs to be done
    Added comments with what I need to do either wednesday or another day

--- Scripts/Manager/Tutorial.cs
+++ Scripts/Manager/Tutorial.cs
@@ -85,7 +85,8 @@ public static class Tutorial
         // setup achievements
         instructions =
         [
-            //Todo looking into what is the better version of these text to keep kids entertained and not feeling bored
+            //Todo 
+            
             // intro
             "Hoi! Mijn naam is Klappy en wij gaan samen een beat maken!",
 
@@ -93,36 +94,41 @@ public static class Tutorial
             "Zie je de rode bollen, dat is de kick ring",
             "Via deze ring kun je een kick geluid toevoegen aan het liedje kijk maar!",
             "Ik heb er net drie op gezet, druk nu op '⏯ Start' om de beat te horen",
-            "Gewledige beat toch?",
+            "",//todo Change the text to not be a question but a statement
             "Probeer het zelf maar eens door er 2 op te zetten door op de bolletjes te klicken",
             "Goed gedaan, nou wil ik wel eens horen wat je gedaan hebt!",
+            //todo add text that you need to stomp with the beat to continue the tutorial
             "Wou super gedaan!, nou ik denk dat we wel een stapje verder kunnen gaan",
 
             // klap ring
             "Dit is de klap ring! Hiermee kun je een klap geluid toevoegen",
             "Ik heb zelf net 2 er in gezet, luister er maar eens naar!",
-            "Leuk toch!",
+            "",//todo Change the text to not be a question but a statement
+            //todo add text that you not only need to add x but also remove x
             "Probeer het zelf maar eens door er 2 neer te zetten",
+            //todo add text that you need to clap with the beat to continue the tutorial
             "Ik ben beniewed laat mij eens horen!",
             "Super goed gedaan, het gaat zo goed ik denk dat we er nog iets bij kunnen doen!",
 
             //groene laag
             "Zie je die groene ring om de beats heen? Die vul je in door met je eigen microphone iets op te nemen!",
-            "Dat kun je doen door eerst op het knopje met de beer te duwen, heb ik nu voor jouw al gedaan :D",
+            "Dat kun je doen door eerst op het knopje met de beer te duwen, heb ik nu voor jouw al gedaan :D", //Todo To be removed
             "Probeer het maar eens door op het microphone icoontje te clicken",
-            "grappig he",
+            "",//todo Change the text to not be a question but a statement
             "Laat eens horen!",
             "Super gedaan, het klinkt enorm leuk",
             
             // chaos pad
-            //Todo Need to add the context to and  the chaos pad Link synth boven stem rechts stem met reverb
+            //Todo Change reverb into galm and change chaos pad into mixer 
             "Ik denk dat we het nog leuker kunnen maken met de chaos pad!",
             "Wat is de chaos pad? Zie je die driehoek links?  Dat is de choas pad!",
             "Hiermee kun je jouw net opgenomen sample veranderen",
             "Je kunt het geluid veranderen naar Boven Jouw stem  Links onder een synth geluid en Rechts onder jouw stem met een revervb",
+            //todo add spefic location to where you need to put the fingerprint button in this case the piano
             "probeer het maar eens door die witte button te bewegen",
             "Moet je nu maar eens luisteren",
             "Leuk toch, brengt een heel nieuwe kijk op het liedje",
+            //todo add specfic location to where you need to put the fingerprint button
             "Het beste is nog als je hem ergens in het midden tussen twee zet wordt het gemixed!",
             "Zo kun je een synth geluid en reverb met stem krijgen",
             //End of tutorial
@@ -134,7 +140,10 @@ public static class Tutorial
 
         conditions =
         [
+            
             // intro
+            
+            //todo Add the clap to continue back into the start of the tutorial
             () => Input.IsActionJustPressed("Interaction"), // need to make a check for button press or screen tap
 
             // rode ring
@@ -143,14 +152,17 @@ public static class Tutorial
             () => BpmManager.instance.playing, // This checks whether the song is playing
             () => !BpmManager.instance.playing, // This checks whether the song is not playing 
             () => activeBeatsPerRing(_indexRedRing) >= _beatsActiveRedRing, // This checks whether the 5 beats are active
+            //Todo add that you need to stomp with the beat to continue the tutorial
             () => BpmManager.instance.playing , // This checks whether the song is playing
             () => BpmManager.instance.playing ==false, 
 
             // oranje ring
             () => Input.IsActionJustPressed("Interaction"), // need to make a check for button press or screen tap
             () => BpmManager.instance.playing, // This checks whether the song is playing
-            () => BpmManager.instance.playing== false, 
+            () => BpmManager.instance.playing== false,
+            //Todo Add that you need to not only add x also remove x
             () => activeBeatsPerRing(_indexOrangeRing) >= _beatsActiveOrangeRing, // Again what is the 4, I assume checks if the 2 rings are active
+            //Todo add that you need to clap with the beat to continue the tutorial
             () => BpmManager.instance.playing, // This checks whether the song is playing
             () => BpmManager.instance.playing ==false,  
 
@@ -165,6 +177,7 @@ public static class Tutorial
             // chaos pad
             ()=>
             {
+                //todo make it so that you need to put it on spefic location instead of any
                 _knobPos = manager.knob.GlobalPosition;
                 return Input.IsActionJustPressed("Interaction");
             }, // need to make a check for button press or screen tap,
@@ -177,11 +190,13 @@ public static class Tutorial
                 return   moved;
             },
             ()=> BpmManager.instance.playing,
+            //todo Add that you need to put it on spefic location with particle effects
             ()=> Input.IsActionJustPressed("Interaction"),
             ()=> Input.IsActionJustPressed("Interaction"),
             ()=> Input.IsActionJustPressed("Interaction"),
             
             // end of tutorial
+           
             () => Input.IsActionJustPressed("Interaction") // need to make a check for button press or screen tap
             
         ];
@@ -214,8 +229,8 @@ public static class Tutorial
 
             }, 
             null,
-            null, // druk play
-            null, // geef energie
+            null, 
+            null,
             null,
             () => { manager.SetGreenLayerVisibility(true); Manager.instance.SynthMixing_ChangeSynth(_greenLayerMicIndex);
                 
@@ -227,7 +242,7 @@ public static class Tutorial
             null,
             null,
             //chaos pad
-            //todo add the 8 levels of the chaos pad
+            //todo add particles effects for the specific location of the fingerprint button
             () =>
             {
                 manager.chaosPadTriangleSprite.Visible = true;
@@ -240,7 +255,7 @@ public static class Tutorial
             null,
             null,
             null,
-            
+            // todo Add autostop for tutorial 
             null
             
         ];

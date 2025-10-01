de02cda013f14e3b3060189777a057d804dcd3a6
MelodyTheMuse <keanodussel@gmail.com>
2025-10-01 16:25:26 +0200

    Updated tutorial to feedback
    Typo's are mostly removed
    The questions in the text have been replaced
    Added clap to start
    Added stomp to the beat
    Added Clap to the beat
    Added the auto stop

--- Scripts/Manager/Tutorial.cs
+++ Scripts/Manager/Tutorial.cs
@@ -23,6 +23,7 @@ public static class Tutorial
     private static int _ringLeft = 4;
     private static int _greenLayerMicIndex = 0;
     private static Vector2 _knobPos;
+    private static int _amountRedRing;
 
     static Manager manager => Manager.instance;
 
@@ -88,46 +89,48 @@ public static class Tutorial
             //Todo 
             
             // intro
-            "Hoi! Mijn naam is Klappy en wij gaan samen een beat maken!",
+            "Hoi! Mijn naam is Klappy en wij gaan samen een beat maken! Om te beginnen klap in je handen.",
 
             // kick ring
-            "Zie je de rode bollen, dat is de kick ring",
+            "Zie je de rode circles, dat is de kick ring",
             "Via deze ring kun je een kick geluid toevoegen aan het liedje kijk maar!",
             "Ik heb er net drie op gezet, druk nu op '⏯ Start' om de beat te horen",
-            "",//todo Change the text to not be a question but a statement
-            "Probeer het zelf maar eens door er 2 op te zetten door op de bolletjes te klicken",
+            "Super nu kunnen wij het lied horen",
+            "Probeer het zelf maar eens door er 2 op te zetten door op de stippen te duwen",
             "Goed gedaan, nou wil ik wel eens horen wat je gedaan hebt!",
             //todo add text that you need to stomp with the beat to continue the tutorial
-            "Wou super gedaan!, nou ik denk dat we wel een stapje verder kunnen gaan",
+            "Stamp eens mee op jouw beat!",
+            "Wow super gedaan!, nou ik denk dat we wel een stapje verder kunnen gaan",
 
             // klap ring
             "Dit is de klap ring! Hiermee kun je een klap geluid toevoegen",
             "Ik heb zelf net 2 er in gezet, luister er maar eens naar!",
-            "",//todo Change the text to not be a question but a statement
+            "Super gedaan",//todo Change the text to not be a question but a statement
             //todo add text that you not only need to add x but also remove x
-            "Probeer het zelf maar eens door er 2 neer te zetten",
+            "Probeer nu eens zelf 2 klap stippen er bij te zetten en een kick weg te halen door er weer op te clicken",
             //todo add text that you need to clap with the beat to continue the tutorial
             "Ik ben beniewed laat mij eens horen!",
+            "Probeer mee te klappen op de beat!",
             "Super goed gedaan, het gaat zo goed ik denk dat we er nog iets bij kunnen doen!",
 
             //groene laag
-            "Zie je die groene ring om de beats heen? Die vul je in door met je eigen microphone iets op te nemen!",
-            "Dat kun je doen door eerst op het knopje met de beer te duwen, heb ik nu voor jouw al gedaan :D", //Todo To be removed
+            "Zie je die groene ring om de stippen heen? Die vul je in door met je eigen microfoon iets op te nemen!",
+            
             "Probeer het maar eens door op het microphone icoontje te clicken",
-            "",//todo Change the text to not be a question but a statement
+            "Goed gedaan",//todo Change the text to not be a question but a statement
             "Laat eens horen!",
             "Super gedaan, het klinkt enorm leuk",
             
             // chaos pad
             //Todo Change reverb into galm and change chaos pad into mixer 
-            "Ik denk dat we het nog leuker kunnen maken met de chaos pad!",
-            "Wat is de chaos pad? Zie je die driehoek links?  Dat is de choas pad!",
+            "Ik denk dat we het nog leuker kunnen maken met de mixer!",
+            "Wat is de mixer? Zie je die driehoek links?  Dat is de mixer!",
             "Hiermee kun je jouw net opgenomen sample veranderen",
-            "Je kunt het geluid veranderen naar Boven Jouw stem  Links onder een synth geluid en Rechts onder jouw stem met een revervb",
+            "Je kunt het geluid veranderen naar Boven Jouw stem Links onder een electronisch geluid en Rechts onder jouw stem met een galm",
             //todo add spefic location to where you need to put the fingerprint button in this case the piano
-            "probeer het maar eens door die witte button te bewegen",
+            "probeer het maar eens door die knop met vinger afdruk te bewegen",
             "Moet je nu maar eens luisteren",
-            "Leuk toch, brengt een heel nieuwe kijk op het liedje",
+            "Leuk dit brengt een heel nieuwe kijk op het liedje",
             //todo add specfic location to where you need to put the fingerprint button
             "Het beste is nog als je hem ergens in het midden tussen twee zet wordt het gemixed!",
             "Zo kun je een synth geluid en reverb met stem krijgen",
@@ -144,31 +147,43 @@ public static class Tutorial
             // intro
             
             //todo Add the clap to continue back into the start of the tutorial
-            () => Input.IsActionJustPressed("Interaction"), // need to make a check for button press or screen tap
+            () => manager.clapped, 
 
             // rode ring
             () => Input.IsActionJustPressed("Interaction"), // need to make a check for button press or screen tap
             () => Input.IsActionJustPressed("Interaction"), // need to make a check for button press or screen tap
             () => BpmManager.instance.playing, // This checks whether the song is playing
-            () => !BpmManager.instance.playing, // This checks whether the song is not playing 
+            ()=> Input.IsActionJustPressed("Interaction"), // need to make a check for button press or screen tap,
             () => activeBeatsPerRing(_indexRedRing) >= _beatsActiveRedRing, // This checks whether the 5 beats are active
             //Todo add that you need to stomp with the beat to continue the tutorial
             () => BpmManager.instance.playing , // This checks whether the song is playing
-            () => BpmManager.instance.playing ==false, 
+            ()=>
+            {
+                GD.Print(manager.stompedAmount);
+                return manager.stompedAmount >= _beatsActiveRedRing;
+            }, // makes sure the amount you stomped is equal to the amount of beats active
+            () => Input.IsActionJustPressed("Interaction"), // need to make a check for button press or screen tap, 
 
             // oranje ring
             () => Input.IsActionJustPressed("Interaction"), // need to make a check for button press or screen tap
             () => BpmManager.instance.playing, // This checks whether the song is playing
-            () => BpmManager.instance.playing== false,
+            () =>
+            {
+                _amountRedRing = activeBeatsPerRing(_indexRedRing);
+               return Input.IsActionJustPressed("Interaction");
+            },
             //Todo Add that you need to not only add x also remove x
-            () => activeBeatsPerRing(_indexOrangeRing) >= _beatsActiveOrangeRing, // Again what is the 4, I assume checks if the 2 rings are active
+            () =>
+            {
+                return activeBeatsPerRing(_indexOrangeRing) >= _beatsActiveOrangeRing && activeBeatsPerRing(_indexRedRing) < _amountRedRing ;
+            }, 
             //Todo add that you need to clap with the beat to continue the tutorial
             () => BpmManager.instance.playing, // This checks whether the song is playing
-            () => BpmManager.instance.playing ==false,  
+            ()=> manager.clappedAmount >= _beatsActiveRedRing, // This checks whether the song is playing
+            () => Input.IsActionJustPressed("Interaction"), // need to make a check for button press or screen tap,   
 
             // layer voice over
             () => Input.IsActionJustPressed("Interaction"), // need to make a check for button press or screen tap
-            () => Input.IsActionJustPressed("Interaction"),
             () => manager.layerVoiceOver0.finished,
             () => Input.IsActionJustPressed("Interaction"), // need to make a check for button press or screen tap
             () => BpmManager.instance.playing,
@@ -202,7 +217,7 @@ public static class Tutorial
         ];
 
         outcomes =
-        [ // todo Setting 3 red beats active
+        [ 
             () =>
             {
                 manager.SetRingVisibility(_indexRedRing, true);
@@ -218,24 +233,24 @@ public static class Tutorial
 
             },
             null,
+            ()=> BpmManager.instance.playing = false,
             null,
             null,
-            null,
-            //Todo setting 2 orange beats active
+            ()=> BpmManager.instance.playing = false,
             () => manager.SetRingVisibility(_indexOrangeRing, true),
             ()=>{
             manager.beatActives[_indexOrangeRing, _ringTop] = true;
             manager.beatActives[_indexOrangeRing, _ringBottom] = true;
 
             }, 
-            null,
+            ()=> BpmManager.instance.playing = false,
             null, 
             null,
             null,
+            ()=> BpmManager.instance.playing = false,
             () => { manager.SetGreenLayerVisibility(true); Manager.instance.SynthMixing_ChangeSynth(_greenLayerMicIndex);
                 
             } ,
-            null,
             null,
             null,
             null,
@@ -256,7 +271,13 @@ public static class Tutorial
             null,
             null,
             // todo Add autostop for tutorial 
-            null
+            () =>
+            {
+                tutorial_level = -2;
+                manager.SetEntireInterfaceVisibility(true);
+                manager.achievementspanel.Visible = false;
+                manager.PlayExtraSFX(manager.achievement_sfx);
+            }
             
         ];
     }
@@ -309,7 +330,6 @@ public static class Tutorial
                 {
                     if (outcome != null) outcome();
                     tutorial_level++;
-                    manager.EmitAchievementParticles();
                     manager.PlayExtraSFX(manager.achievement_sfx);
                     SpeakTutorialInstruction(tutorial_level);
                 }

49ef8f5b66504a5aa183c8ce6183b3e51163dd7d
MelodyTheMuse <keanodussel@gmail.com>
2025-11-18 15:00:25 +0100

    Klappy talks with tts
    Klappy has an animation that will makes his mouth move it now is played when tts is talking

--- Experimental/Klappy/klappy_animations.gd
+++ Experimental/Klappy/klappy_animations.gd
@@ -2,12 +2,14 @@ extends Node3D
 
 @onready var animation_tree = $model/AnimationTree
 
+var talking = false
 var beat_time = 0.0
 
 func _ready():
 	# set animation to end to prevent playing on start
 	animation_tree.set("parameters/ClapTrigger/seek_request", 10000.0)
 	animation_tree.set("parameters/StampTrigger/seek_request", 10000.0)
+	animation_tree.set("parameters/talkingTrigger/seek_request", 10000.0)
 	
 	# default speed for 120 bpm
 	if beat_time == 0:
@@ -21,6 +23,12 @@ func _input(event):
 		if event.keycode == KEY_SPACE:
 			on_clap()
 			on_stamp()
+			on_talking()
+
+func _process(_delta: float) -> void:
+	if talking: return
+	if DisplayServer.tts_is_speaking():
+			on_talking()
 
 # trigger clap animation by setting time to 0.0
 func on_clap():
@@ -30,6 +38,14 @@ func on_clap():
 # trigger stamp animation by setting time to 0.0
 func on_stamp():
 	animation_tree.set("parameters/StampTrigger/seek_request", 0)
+
+func on_talking():
+	animation_tree.set("parameters/talkingTrigger/seek_request",0)
+	talking = true
+	get_tree().create_timer(0.7).timeout.connect(_on_timeout)
+
+func _on_timeout():
+	talking = false
 
 # adjust animation speed to match bpm
 func on_bpm_changed(bpm:float):

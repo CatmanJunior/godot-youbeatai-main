d58256a831fabd14fe849022a75235429403ea44
esmee veldhuizen <esmee.veldhuizen@outlook.com>
2026-02-09 12:24:31 +0100

    Start of klappys audio effect
    no need to merge this yet or anything its literally nothing, just klappys light responding to wasd

---
+++ Scripts/KlappyAudioEffect.cs
@@ -0,0 +1,13 @@
+using Godot;
+
+public partial class KlappyAudioEffect : Sprite2D
+{
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+    }
+
+    public override void _Process(double delta)
+    {
+    }
+}

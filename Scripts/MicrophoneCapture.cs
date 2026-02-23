24d4642e85562c32e4f045b86f8e40099527ee1e
Jesse van Leeuwen <sf.jessenl@gmail.com>
2026-02-23 10:28:08 +0100

    feat: mic input on another thread

--- Scripts/MicrophoneCapture.cs
+++ Scripts/MicrophoneCapture.cs
@@ -1,210 +1,227 @@
 using Godot;
 using System;
 using System.Numerics;
+using System.Threading;
 
 public partial class MicrophoneCapture : Node
 {
-    public static MicrophoneCapture instance = null;
+	public static MicrophoneCapture instance = null;
 
-    public override void _ExitTree()
-    {
-        if (instance == this) instance = null;
-    }
+	public override void _ExitTree()
+	{
+		if (instance == this) instance = null;
+	}
 
-    [Export] string busName = "Microphone";
+	[Export] string busName = "Microphone";
 
-    [Export] private float clapFreq = 7000.0f;
-    [Export] private float clapthreshold = 0.1f;
-    [Export] private float stampFreq = 30.0f;
-    [Export] private float stampThreshold = 0.005f;
+	[Export] private float clapFreq = 7000.0f;
+	[Export] private float clapthreshold = 0.1f;
+	[Export] private float stampFreq = 30.0f;
+	[Export] private float stampThreshold = 0.005f;
 
 
-    private AudioEffectCapture audioEffectCapture;
-    private AudioStreamPlayer audioStreamPlayer;
-    private AudioEffectSpectrumAnalyzerInstance analyzer;
-    private AudioStream microphone;
+	private AudioEffectCapture audioEffectCapture;
+	private AudioStreamPlayer audioStreamPlayer;
+	private AudioEffectSpectrumAnalyzerInstance analyzer;
+	private AudioStream microphone;
 
-    public float volume = 0;
-    public float frequency = 0;
+	public float volume = 0;
+	public float frequency = 0;
 
-    public double clapVolume = 0;
-    public double stampVolume = 0;
+	public double clapVolume = 0;
+	public double stampVolume = 0;
 
-    public bool IsClapping => clapVolume > clapthreshold && clapVolume > stampVolume;
-    public bool IsStamping => stampVolume > stampThreshold && stampVolume > clapVolume;
+	public bool IsClapping => clapVolume > clapthreshold && clapVolume > stampVolume;
+	public bool IsStamping => stampVolume > stampThreshold && stampVolume > clapVolume;
 
-    [Signal]
-    public delegate void OnMicrophoneInputEventHandler(float volume, float frequency);
+	[Signal]
+	public delegate void OnMicrophoneInputEventHandler(float volume, float frequency);
 
-    public override void _Ready()
-    {
-        instance ??= this;
+	private Godot.Mutex mutex;
+	private GodotThread thread;
 
-        microphone = new AudioStreamMicrophone();
+	public override void _Ready()
+	{
+		instance ??= this;
 
-        audioStreamPlayer = new AudioStreamPlayer();
-        AddChild(audioStreamPlayer);
-        audioStreamPlayer.Finished += () => audioStreamPlayer.Play();
+		microphone = new AudioStreamMicrophone();
 
-        audioStreamPlayer.Stream = microphone;
-        analyzer = (AudioEffectSpectrumAnalyzerInstance)AudioServer.GetBusEffectInstance(AudioServer.GetBusIndex(busName), 2);
+		audioStreamPlayer = new AudioStreamPlayer();
+		AddChild(audioStreamPlayer);
+		audioStreamPlayer.Finished += () => audioStreamPlayer.Play();
 
-        audioStreamPlayer.Bus = busName;
-        audioEffectCapture = (AudioEffectCapture)AudioServer.GetBusEffect(AudioServer.GetBusIndex(busName), 0);
+		audioStreamPlayer.Stream = microphone;
+		analyzer = (AudioEffectSpectrumAnalyzerInstance)AudioServer.GetBusEffectInstance(AudioServer.GetBusIndex(busName), 2);
 
-        audioStreamPlayer.Play();
-    }
+		audioStreamPlayer.Bus = busName;
+		audioEffectCapture = (AudioEffectCapture)AudioServer.GetBusEffect(AudioServer.GetBusIndex(busName), 0);
 
-    public override void _Process(double delta)
-    {
-        var frames = audioEffectCapture.GetFramesAvailable();
-        if (frames > 0)
-        {
-            Godot.Vector2[] audioData = audioEffectCapture.GetBuffer(frames);
-            volume = GetVolumeFromAudioData(audioData);
-            frequency = GetPeakFrequency(PerformFFT(ConvertToMono(audioData)));
+		audioStreamPlayer.Play();
 
-            // Call signal
-            EmitSignal(SignalName.OnMicrophoneInput, volume, frequency);
-        }
+		thread = new GodotThread();
+		thread.Start(new Callable(this, nameof(ThreadedProcess)), GodotThread.Priority.Normal);
+	}
 
-        stampVolume = ConvertTo01(analyzer.GetMagnitudeForFrequencyRange(0, stampFreq));
-        clapVolume = ConvertTo01(analyzer.GetMagnitudeForFrequencyRange(clapFreq, 20000));
+	private void ThreadedProcess()
+	{
+		while(IsQueuedForDeletion() == false)
+		{
+			GD.Print("hoi");
 
-        // if needed we can connect the tresholds to the old treshold slider in the settings menu - Sjoerd 18 jan 2026
-        // for both Stamp and Clap need different thresholds on their frequency ranges, 2 separate sliders would work beter - jesse 16 feb 2026
-        /*
-            clapthreshold = (float)Manager.instance.volume_treshold.Value;
-            stampThreshold = (float)Manager.instance.volume_treshold.Value;
-        */
+			var frames = audioEffectCapture.GetFramesAvailable();
+			if (frames > 0)
+			{
+				Godot.Vector2[] audioData = audioEffectCapture.GetBuffer(frames);
+				volume = GetVolumeFromAudioData(audioData);
+				frequency = GetPeakFrequency(PerformFFT(ConvertToMono(audioData)));
+			}
 
-        // GD.Print($"stamp volume: {stampVolume}");
-        // GD.Print($"clap volume: {clapVolume}");
-        // GD.Print($"clap treshold: {clapthreshold}");
-        // GD.Print($"stamp treshold: {stampThreshold}");
-    }
+			Thread.Sleep(20);
+		}
+	}
 
-    private double ConvertTo01(Godot.Vector2 rms)
-    {
-        float rms_value = (rms.X + rms.Y) * 0.5f;
-        double log_value = 20.0 * (Math.Log(Math.Sqrt(rms_value) / 0.1) / Math.Log(10));
+	public override void _Process(double delta)
+	{
+		// Call signal
+		EmitSignal(SignalName.OnMicrophoneInput, volume, frequency);
 
-        // convert to value around 0-1
-        return Math.Pow(10, log_value / 10);
-    }
+		stampVolume = ConvertTo01(analyzer.GetMagnitudeForFrequencyRange(0, stampFreq));
+		clapVolume = ConvertTo01(analyzer.GetMagnitudeForFrequencyRange(clapFreq, 20000));
 
-    private float[] ConvertToMono(Godot.Vector2[] audioData)
-    {
-        // Convert stereo (Vector2) to mono by averaging the left and right channels (X and Y)
-        float[] monoData = new float[audioData.Length];
-        for (int i = 0; i < audioData.Length; i++)
-        {
-            monoData[i] = (audioData[i].X + audioData[i].Y) / 2f; // Average the stereo channels
-        }
-        return monoData;
-    }
+		// if needed we can connect the tresholds to the old treshold slider in the settings menu - Sjoerd 18 jan 2026
+		// for both Stamp and Clap need different thresholds on their frequency ranges, 2 separate sliders would work beter - jesse 16 feb 2026
+		/*
+			clapthreshold = (float)Manager.instance.volume_treshold.Value;
+			stampThreshold = (float)Manager.insta   nce.volume_treshold.Value;
+		*/
 
-    private float GetVolumeFromAudioData(Godot.Vector2[] audioData)
-    {
-        float sum = 0f;
-        foreach (Godot.Vector2 sample in audioData)
-        {
-            float averageSample = (Mathf.Abs(sample.X) + Mathf.Abs(sample.Y)) / 2;
-            sum += averageSample;
-        }
-        return sum / audioData.Length;
-    }
+		// GD.Print($"stamp volume: {stampVolume}");
+		// GD.Print($"clap volume: {clapVolume}");
+		// GD.Print($"clap treshold: {clapthreshold}");
+		// GD.Print($"stamp treshold: {stampThreshold}");
+	}
 
-    private int GetNextPowerOf2(int n)
-    {
-        // Find the next power of 2 greater than or equal to n
-        int powerOf2 = 1;
-        while (powerOf2 < n)
-        {
-            powerOf2 <<= 1;  // Double the value (same as powerOf2 *= 2)
-        }
-        return powerOf2;
-    }
+	private double ConvertTo01(Godot.Vector2 rms)
+	{
+		float rms_value = (rms.X + rms.Y) * 0.5f;
+		double log_value = 20.0 * (Math.Log(Math.Sqrt(rms_value) / 0.1) / Math.Log(10));
 
-    private float[] PerformFFT(float[] audioData)
-    {
-        // Check if the data length is a power of 2
-        int N = audioData.Length;
-        int nextPowerOf2 = GetNextPowerOf2(N);
+		// convert to value around 0-1
+		return Math.Pow(10, log_value / 10);
+	}
 
-        // If the length is not a power of 2, pad with zeros
-        if (N != nextPowerOf2)
-        {
-            float[] paddedData = new float[nextPowerOf2];
-            Array.Copy(audioData, paddedData, N);  // Copy original data into padded array
-            audioData = paddedData;  // Update audioData to be padded
-        }
+	private float[] ConvertToMono(Godot.Vector2[] audioData)
+	{
+		// Convert stereo (Vector2) to mono by averaging the left and right channels (X and Y)
+		float[] monoData = new float[audioData.Length];
+		for (int i = 0; i < audioData.Length; i++)
+		{
+			monoData[i] = (audioData[i].X + audioData[i].Y) / 2f; // Average the stereo channels
+		}
+		return monoData;
+	}
 
-        // Convert audioData to complex numbers
-        Complex[] complexData = new Complex[audioData.Length];
-        for (int i = 0; i < audioData.Length; i++)
-        {
-            complexData[i] = new Complex(audioData[i], 0);  // Imaginary part is 0
-        }
+	private float GetVolumeFromAudioData(Godot.Vector2[] audioData)
+	{
+		float sum = 0f;
+		foreach (Godot.Vector2 sample in audioData)
+		{
+			float averageSample = (Mathf.Abs(sample.X) + Mathf.Abs(sample.Y)) / 2;
+			sum += averageSample;
+		}
+		return sum / audioData.Length;
+	}
 
-        // Perform the FFT
-        FFT(complexData);
+	private int GetNextPowerOf2(int n)
+	{
+		// Find the next power of 2 greater than or equal to n
+		int powerOf2 = 1;
+		while (powerOf2 < n)
+		{
+			powerOf2 <<= 1;  // Double the value (same as powerOf2 *= 2)
+		}
+		return powerOf2;
+	}
 
-        // Calculate magnitudes from complex numbers
-        float[] magnitudes = new float[audioData.Length / 2];
-        for (int i = 0; i < audioData.Length / 2; i++)
-        {
-            magnitudes[i] = (float)complexData[i].Magnitude;
-        }
+	private float[] PerformFFT(float[] audioData)
+	{
+		// Check if the data length is a power of 2
+		int N = audioData.Length;
+		int nextPowerOf2 = GetNextPowerOf2(N);
 
-        return magnitudes;
-    }
+		// If the length is not a power of 2, pad with zeros
+		if (N != nextPowerOf2)
+		{
+			float[] paddedData = new float[nextPowerOf2];
+			Array.Copy(audioData, paddedData, N);  // Copy original data into padded array
+			audioData = paddedData;  // Update audioData to be padded
+		}
 
-    private void FFT(Complex[] data)
-    {
-        int N = data.Length;
-        if (N <= 1) return;
+		// Convert audioData to complex numbers
+		Complex[] complexData = new Complex[audioData.Length];
+		for (int i = 0; i < audioData.Length; i++)
+		{
+			complexData[i] = new Complex(audioData[i], 0);  // Imaginary part is 0
+		}
 
-        // Split the data into even and odd parts
-        Complex[] even = new Complex[N / 2];
-        Complex[] odd = new Complex[N / 2];
+		// Perform the FFT
+		FFT(complexData);
 
-        for (int i = 0; i < N / 2; i++)
-        {
-            even[i] = data[2 * i];
-            odd[i] = data[2 * i + 1];
-        }
+		// Calculate magnitudes from complex numbers
+		float[] magnitudes = new float[audioData.Length / 2];
+		for (int i = 0; i < audioData.Length / 2; i++)
+		{
+			magnitudes[i] = (float)complexData[i].Magnitude;
+		}
 
-        // Recursively apply FFT to the even and odd parts
-        FFT(even);
-        FFT(odd);
+		return magnitudes;
+	}
 
-        // Combine the results
-        for (int i = 0; i < N / 2; i++)
-        {
-            Complex t = Complex.Exp(new Complex(0, -2 * Math.PI * i / N)) * odd[i];
-            data[i] = even[i] + t;
-            data[i + N / 2] = even[i] - t;
-        }
-    }
+	private void FFT(Complex[] data)
+	{
+		int N = data.Length;
+		if (N <= 1) return;
 
-    private float GetPeakFrequency(float[] frequencySpectrum)
-    {
-        // Find the index of the highest value in the frequency spectrum array
-        int peakIndex = 0;
-        float maxValue = 0f;
-        for (int i = 0; i < frequencySpectrum.Length; i++)
-        {
-            if (frequencySpectrum[i] > maxValue)
-            {
-                maxValue = frequencySpectrum[i];
-                peakIndex = i;
-            }
-        }
+		// Split the data into even and odd parts
+		Complex[] even = new Complex[N / 2];
+		Complex[] odd = new Complex[N / 2];
 
-        // Assuming you know the sample rate, calculate the frequency based on the index
-        float sampleRate = 44100f; // Set to the appropriate sample rate of your microphone input
-        float frequency = peakIndex * (sampleRate / frequencySpectrum.Length);
-        return frequency;
-    }
+		for (int i = 0; i < N / 2; i++)
+		{
+			even[i] = data[2 * i];
+			odd[i] = data[2 * i + 1];
+		}
+
+		// Recursively apply FFT to the even and odd parts
+		FFT(even);
+		FFT(odd);
+
+		// Combine the results
+		for (int i = 0; i < N / 2; i++)
+		{
+			Complex t = Complex.Exp(new Complex(0, -2 * Math.PI * i / N)) * odd[i];
+			data[i] = even[i] + t;
+			data[i + N / 2] = even[i] - t;
+		}
+	}
+
+	private float GetPeakFrequency(float[] frequencySpectrum)
+	{
+		// Find the index of the highest value in the frequency spectrum array
+		int peakIndex = 0;
+		float maxValue = 0f;
+		for (int i = 0; i < frequencySpectrum.Length; i++)
+		{
+			if (frequencySpectrum[i] > maxValue)
+			{
+				maxValue = frequencySpectrum[i];
+				peakIndex = i;
+			}
+		}
+
+		// Assuming you know the sample rate, calculate the frequency based on the index
+		float sampleRate = 44100f; // Set to the appropriate sample rate of your microphone input
+		float frequency = peakIndex * (sampleRate / frequencySpectrum.Length);
+		return frequency;
+	}
 }

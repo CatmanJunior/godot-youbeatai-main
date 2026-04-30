675e72c55dda1203e79d1f84122bbbf34797179b
esmee veldhuizen <esmee.veldhuizen@outlook.com>
2026-04-30 16:21:20 +0200

    Soundbank klappy talks and checkmarks better

--- Scripts/Klappy/Klappy_soundbank_screen.gd
+++ Scripts/Klappy/Klappy_soundbank_screen.gd
@@ -1,12 +1,14 @@
 extends Node
 @onready var klappy_response = $"../Klappy respons bubble"
 @export var message:String
+@onready var talking = $"../Robot/SubViewportContainer/SubViewport/Klappy"
 
 func _ready() -> void:
 	klappy_response.change_panel_visibility(true)
 	klappy_response.fill_response_label(message)
 	speak()
-
+	talking.talking = true
+	
 func speak():
 	TTSHelper.speak(message)
 
@@ -17,3 +19,4 @@ func _process(_delta: float) -> void:
 func _on_timeout():
 	klappy_response.change_panel_visibility(false)
 	DisplayServer.tts_stop()
+	talking.talking = false

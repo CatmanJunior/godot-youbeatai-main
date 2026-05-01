127166b6947f0b9414d6b33ebdae7cac578004bc
Jesse <sf.jessenl@gmail.com>
2026-05-01 09:38:45 +0200

    fix: correct credits_panel variable declaration and remove unnecessary print statement

--- Scripts/UI/main_menu_panel.gd
+++ Scripts/UI/main_menu_panel.gd
@@ -4,7 +4,7 @@ extends Panel
 @export var credits_button: Button
 @export var back_button: Button
 
-@export var credits_panel = Panel
+@export var credits_panel: Panel
 
 func _ready():
 	pro_button.pressed.connect(_on_pro_button_pressed)
@@ -14,10 +14,8 @@ func _ready():
 func _on_pro_button_pressed():
 	SceneChanger.go_to_soundbank()
 
-
 func _on_credits_button_pressed():
 	credits_panel.visible = true
 
 func _on_back_button_pressed():
-	print("uhh")
 	credits_panel.visible = false

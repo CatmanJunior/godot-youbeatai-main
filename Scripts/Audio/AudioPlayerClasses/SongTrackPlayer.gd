c5b013d536a139e07dc25bb24eddcedd1864badd
Jesse van Leeuwen <sf.jessenl@gmail.com>
2026-05-04 18:16:52 +0200

    Fix: next section only when song mode is active

--- Scripts/Audio/AudioPlayerClasses/SongTrackPlayer.gd
+++ Scripts/Audio/AudioPlayerClasses/SongTrackPlayer.gd
@@ -86,7 +86,7 @@ func setup(index: int, parent_bus: String, _settings : ChordPlayerSettings = nul
 	add_child(chords)
 
 func pre_on_beat(beat:int):
-	if beat == 0:
+	if beat == 0 and _is_playing:
 		EventBus.section_next_requested.emit()
 
 func _on_beat_triggered(_beat: int) -> void:

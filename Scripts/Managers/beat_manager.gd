a81e85cd16936be61d2e708810b42df7bedb5c3e
Rene 't Hart <rjm.thart@gmail.com>
2026-05-07 12:11:12 +0200

    fix: adjust beat sprite click handler logic for energy checks and state management

--- Scripts/Managers/beat_manager.gd
+++ Scripts/Managers/beat_manager.gd
@@ -123,18 +123,18 @@ func _get_swing_offset() -> float:
 # --- Beat manager functions ---
 func _on_beat_sprite_clicked(p_track: int, beat: int):
 	var was_active = get_beat(p_track, beat)
-		
-	if not AchievementManager.has_energy_for_beat_addition() and not was_active or GameState.use_achievements:
-		EventBus.beat_state_changed.emit(p_track, beat, was_active) 
-		EventBus.not_enough_energy.emit()
-		return
-	
+	if GameState.use_achievements:
+		if not AchievementManager.has_energy_for_beat_addition() and not was_active:
+			EventBus.beat_state_changed.emit(p_track, beat, was_active) 
+			EventBus.not_enough_energy.emit()
+			return
+
 	if not was_active:
-		_set_beat(p_track, beat, false)
+		_set_beat(p_track, beat, true)
 		EventBus.play_track_requested.emit(p_track)
 		EventBus.particles_requested.emit(Vector2.ZERO, track_settings_registry.get_track(p_track).track_color)
 	else:
-		_set_beat(p_track, beat, true)
+		_set_beat(p_track, beat, false)
 	EventBus.track_selected.emit(p_track)
 
 func _set_beat(track: int, beat: int, active: bool):

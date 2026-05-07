08f47cc14cdf4d22e7873a65c060698dc7743871
Jesse van Leeuwen <sf.jessenl@gmail.com>
2026-05-07 20:46:34 +0200

    Feat: metronome

---
+++ Scripts/Audio/metronome.gd
@@ -0,0 +1,12 @@
+class_name Metronome
+extends AudioStreamPlayer
+
+
+func _ready():
+    EventBus.beat_triggered.connect(on_beat)
+
+func on_beat(beat:int):
+    if not GameState.metronome_enabled:
+        return
+
+    play()

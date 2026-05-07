5fbd9456b4fb64098c7317ae94057680ab68c336
Jesse van Leeuwen <sf.jessenl@gmail.com>
2026-05-07 12:55:57 +0200

    Fix: disable tutorial when clicking pro modes

--- Scripts/UI/main_menu_panel.gd
+++ Scripts/UI/main_menu_panel.gd
@@ -18,7 +18,7 @@ func _on_tutorial_button_pressed():
 	_on_pro_button_pressed()
 
 func _on_pro_button_pressed():
-	GameState.use_tutorial = true
+	GameState.use_tutorial = false
 	if OS.has_feature("web"):
 		TTSHelper.init()
 		

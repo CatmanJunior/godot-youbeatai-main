3a90a06460a944da41013205ab8cd3970c0c0b7a
Jesse van Leeuwen <sf.jessenl@gmail.com>
2026-05-07 22:42:18 +0200

    Fix: tutorial not starting

--- Scripts/UI/main_menu_panel.gd
+++ Scripts/UI/main_menu_panel.gd
@@ -11,14 +11,14 @@ func _ready():
 	pro_button.pressed.connect(_on_pro_button_pressed)
 	credits_button.pressed.connect(_on_credits_button_pressed)
 	back_button.pressed.connect(_on_back_button_pressed)
-
+	GameState.use_tutorial = false
 
 func _on_tutorial_button_pressed():
 	GameState.use_tutorial = true
 	_on_pro_button_pressed()
 
 func _on_pro_button_pressed():
-	GameState.use_tutorial = false
+	
 	if OS.has_feature("web"):
 		TTSHelper.init()
 		

2df00099e0104ad052e88a49857b5ce73191148c
esmee veldhuizen <esmee.veldhuizen@outlook.com>
2026-05-11 16:42:10 +0200

    fix: idk read description
    - typos in tutorial fixed
    - when locked item unlocked, klappy explains what it is (did not test all of them)
    - chaos pad mic lightup is bigger

--- Scripts/Klappy/klappy_reaction.gd
+++ Scripts/Klappy/klappy_reaction.gd
@@ -3,8 +3,9 @@ extends Node
 
 func _ready() -> void:
 	if not GameState.tutorial_activated:
-		EventBus.achievement_done.connect(_on_achievement_done)
-		EventBus.utterance_ended.connect(_on_utterance_end)
+		_connect_signals()
+	else:
+		EventBus.on_tutorial_done.connect(_on_tutorial_done)
 
 func _on_achievement_done(i):
 	if GameState.achievements_active:
@@ -16,6 +17,15 @@ func _on_achievement_done(i):
 			4:  _fill_instruction_label("Oke nu gaat het echte werk beginnen met de 🎵 Song Mode, alle rondjes worden achter elkaar afgespeeld, en met de microfoon kan je een hele lange opname maken")
 			5:  _fill_instruction_label("Wat een leuke sample, daar krijg ik energie ⚡ van !")
 			6: _fill_instruction_label( "Haha leuk! sleep nu de vingerafdruk naar de 🎤 op de mixer dan hoor je jou sample in de beat ring")
+
+
+func _on_tutorial_done() -> void:
+	_connect_signals()
+
+func _connect_signals() -> void:
+	EventBus.achievement_done.connect(_on_achievement_done)
+	EventBus.utterance_ended.connect(_on_utterance_end)
+
 
 func _fill_instruction_label(_name:String):
 	EventBus.set_klappy_speech_bubble.emit(_name, "", false)

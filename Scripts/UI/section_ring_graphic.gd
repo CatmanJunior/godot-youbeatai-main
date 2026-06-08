ac5a3001dabeaaa0606b5f21f80fc0cdf90bac26
Jesse van Leeuwen <sf.jessenl@gmail.com>
2026-06-08 10:12:50 +0200

    Art: pulse on repeat change for section buttons

--- Scripts/UI/section_ring_graphic.gd
+++ Scripts/UI/section_ring_graphic.gd
@@ -5,13 +5,26 @@ class_name SectionRingGraphic
 @export var repeating: int # the amount of filled shapes
 @export var fill: float # progress to the next shape
 
+@export var pulse_on_change: bool = false
+@export var pulse_color: Color
+var default_color: Color
+
 func _ready():
+    default_color = self_modulate
     fill_material = material as ShaderMaterial
     update_fill_shader() # setup first values
 
 func set_repeating(value: int) -> void:
+    var changed:= value != repeating
     repeating = value
     update_fill_shader()
+
+    if pulse_on_change and changed:
+        var tween = create_tween()
+        tween.tween_property(self, "self_modulate", pulse_color, 0.2)
+        tween.tween_property(self, "self_modulate", default_color, 0.3)
+        return
+
 
 func set_fill(value: float):
     fill = min(value, 1)
